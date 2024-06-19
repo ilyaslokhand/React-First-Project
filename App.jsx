9a50d8c461776bc68e831a1f0d7bf0576a2463d4
ilyas lokhand <ilokhandwala786@gmail.com>
2024-06-19 12:47:03 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="containor">
+        <div className="text">
+          <h1>
+            The Westmire <br></br>
+            <span>A56 Headset</span>
+          </h1>
+          <div className="buttons">
+            <button id="buy">Buy Now</button>
+            <button id="show">Show All</button>
+          </div>
+        </div>
+        <div className="image">
+          <img src="https://www.pngall.com/wp-content/uploads/4/Headphone-Download-Free-PNG.png"></img>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
